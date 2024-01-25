2ee251fc69a9f16a73c4a47cc63d3c861c517b66
Gustaman <>
2024-01-25 16:03:47 +0700

    Color schemes

---
+++ widgets/colors_x.swift
@@ -0,0 +1,12 @@
+class ColorX {
+    static let blue = UIColor(hex: 0x007bff)
+    static let teal = UIColor(hex: 0x17a2b8)
+    static let green = UIColor(hex: 0x28a745)
+    static let yellow = UIColor(hex: 0xffc107)
+    static let red = UIColor(hex: 0xdc3545)
+    static let black = UIColor(hex: 0x343a40)
+    static let gray = UIColor(hex: 0x6c757d)
+    static let transparent = UIColor.clear
+    static let white = UIColor.white
+    static let highlight = UIColor(hex: 0xE3E3E3)
+}
